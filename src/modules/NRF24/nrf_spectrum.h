c84ba94a41b9421cb0be26315cf114429ceb9ec1
Dnzeet <132293917+Dnzeet@users.noreply.github.com>
2026-08-03 09:38:22 +0700

    Add files via upload

--- src/modules/NRF24/nrf_spectrum.h
+++ src/modules/NRF24/nrf_spectrum.h
@@ -3,6 +3,9 @@
 #include "modules/NRF24/nrf_common.h"
 #include <RF24.h>
 
+// nRF24 covers channels 0-125 (2.400-2.525 GHz), 126 channels total
+#define NRF_SPECTRUM_CHANNELS 126
+
 void nrf_spectrum();
 
 String scanChannels(bool web = false);
