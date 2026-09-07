420999d9801d1bddbbf866c42655afc5f91752f4
Dnzeet <132293917+Dnzeet@users.noreply.github.com>
2026-09-07 12:16:01 +0700

    Add files via upload

--- src/core/menu_items/IRMenu.cpp
+++ src/core/menu_items/IRMenu.cpp
@@ -13,14 +13,13 @@ void IRMenu::optionsMenu() {
     M5.Power.setExtOutput(true); // ENABLE 5V OUTPUT
 #endif
     options = {
-        {"TV-B-Gone",     StartTvBGone              },
-        {"Custom IR",     otherIRcodes              },
-        {"Timed Transmit", timedIrTransmitMenu       },
-        {"IR Read",       [=]() { IrRead(); }       },
+        {"TV-B-Gone", StartTvBGone              },
+        {"Custom IR", otherIRcodes              },
+        {"IR Read",   [=]() { IrRead(); }       },
 #if !defined(LITE_VERSION)
-        {"IR Jammer",     startIrJammer             }, // Simple frequency-adjustable jammer
+        {"IR Jammer", startIrJammer             }, // Simple frequency-adjustable jammer
 #endif
-        {"Config",        [this]() { configMenu(); }},
+        {"Config",    [this]() { configMenu(); }},
     };
     addOptionToMainMenu();
 
